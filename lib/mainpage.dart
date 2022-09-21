cba467e215a6280b683e2e49f9a2f8a66b487e86
wogks <wogks@wogksui-MacBookPro.local>
2022-09-21 17:59:08 +0900

    Feat: sensor 연습

---
+++ lib/mainpage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  double dx = 100;
+  double dy = 100;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<GyroscopeEvent>(
+        stream: SensorsPlatform.instance.gyroscopeEvents,
+        builder: (_, snapshot) {
+          if(snapshot.hasData) {
+            dx = dx + snapshot.data!.y;
+            dy = dy + snapshot.data!.x;
+          }
+          return Transform.translate(
+            offset: Offset(dx, dy),
+            child: const CircleAvatar(
+              radius: 20,
+            ),
+          );
+        },
+      )
+    );
+  }
+}
